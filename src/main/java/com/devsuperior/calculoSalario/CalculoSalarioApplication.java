de2073cb2096077928fab11c3c84bd62327e8e1a
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-01-28 18:21:42 -0300

    Initial commit

---
+++ src/main/java/com/devsuperior/calculoSalario/CalculoSalarioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.calculoSalario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoSalarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoSalarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/calculoSalario/CalculoSalarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.calculoSalario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoSalarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
